2730cebd92f43bb89f1166e3b035e7ff399d125b
Stephen Stack <stephen.stack@smartthings.com>
2026-09-02 14:29:53 -0500

    Init

---
+++ exercises/01_variables/variables2.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // TODO: Change the line below to fix the compiler error.
+    let x: u8;
+
+    x = 3;
+
+    if x == 10 {
+        println!("x is ten!");
+    } else {
+        println!("x is not ten!");
+    }
+}
