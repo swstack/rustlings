805123e5feba6350d6ef1433845da0ab08e75de3
Stephen Stack <stephen.stack@smartthings.com>
2026-09-03 09:58:30 -0700

    solution: move semantics

--- exercises/06_move_semantics/move_semantics1.rs
+++ exercises/06_move_semantics/move_semantics1.rs
@@ -1,6 +1,6 @@
 // TODO: Fix the compiler error in this function.
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let vec = vec;
+    let mut vec = vec;
 
     vec.push(88);
 
@@ -18,7 +18,8 @@ mod tests {
     #[test]
     fn move_semantics1() {
         let vec0 = vec![22, 44, 66];
-        let vec1 = fill_vec(vec0);
+        let vec1 = fill_vec(vec0.clone());
         assert_eq!(vec1, vec![22, 44, 66, 88]);
+        assert_eq!(vec0, vec![22, 44, 66]);
     }
 }
