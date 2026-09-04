032bb1d03ba83a306f2177ad3d65b0f87632eb31
Stephen Stack <stephen.stack@smartthings.com>
2026-09-04 14:48:13 -0700

    solution: quiz2

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,22 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut results = vec![];
+
+        for (mut s, command) in input {
+            match command {
+                Command::Uppercase => results.push(s.to_uppercase()),
+                Command::Trim => results.push(s.trim().to_string()),
+                Command::Append(times) => {
+                    s.push_str(&"bar".repeat(times));
+                    results.push(s)
+                },
+            }
+        }
+
+        results
+    }
 }
 
 fn main() {
@@ -37,7 +52,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
