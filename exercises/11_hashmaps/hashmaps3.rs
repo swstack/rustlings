d7fa77d61fe1cbb465c9d2e0ba7a7c52f8a52780
Stephen Stack <stephen.stack@smartthings.com>
2026-09-04 14:32:36 -0700

    solution: hashmaps

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -19,18 +19,51 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
     // The name of the team is the key and its associated struct is the value.
     let mut scores = HashMap::<&str, TeamScores>::new();
 
-    for line in results.lines() {
+    for line in results.trim().lines() {
+
         let mut split_iterator = line.split(',');
         // NOTE: We use `unwrap` because we didn't deal with error handling yet.
         let team_1_name = split_iterator.next().unwrap();
         let team_2_name = split_iterator.next().unwrap();
-        let team_1_score: u8 = split_iterator.next().unwrap().parse().unwrap();
-        let team_2_score: u8 = split_iterator.next().unwrap().parse().unwrap();
+
+        let team_1_scored: u8 = split_iterator.next().unwrap().parse().unwrap();
+        let team_2_scored: u8 = split_iterator.next().unwrap().parse().unwrap();
+        let team_1_conceded = team_2_scored;
+        let team_2_conceded = team_1_scored;
 
         // TODO: Populate the scores table with the extracted details.
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
+
+        if !scores.contains_key(team_1_name) {
+            scores.insert(
+                team_1_name,
+                TeamScores {
+                    goals_scored: 0,
+                    goals_conceded: 0,
+                },
+            );
+        }
+
+        let team_1_scores = scores.get_mut(team_1_name).unwrap();
+        team_1_scores.goals_scored += team_1_scored;
+        team_1_scores.goals_conceded += team_1_conceded;
+
+        if !scores.contains_key(team_2_name) {
+            scores.insert(
+                team_2_name,
+                TeamScores {
+                    goals_scored: 0,
+                    goals_conceded: 0,
+                },
+            );
+        }
+
+        let team_2_scores = scores.get_mut(team_2_name).unwrap();
+        team_2_scores.goals_scored += team_2_scored;
+        team_2_scores.goals_conceded += team_2_conceded;
+
     }
 
     scores
@@ -44,7 +77,8 @@ fn main() {
 mod tests {
     use super::*;
 
-    const RESULTS: &str = "England,France,4,2
+    const RESULTS: &str = "
+England,France,4,2
 France,Italy,3,1
 Poland,Spain,2,0
 Germany,England,2,1
