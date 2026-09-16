44e574481e2d4b87b66f99b0215a0dc9dd2021bd
Stephen Stack <stephen.stack@smartthings.com>
2026-09-16 16:41:06 -0500

    solution: macros

--- exercises/21_macros/macros3.rs
+++ exercises/21_macros/macros3.rs
@@ -1,11 +1,13 @@
 // TODO: Fix the compiler error without taking the macro definition out of this
 // module.
+#[macro_use]
 mod macros {
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
         };
     }
+
 }
 
 fn main() {
